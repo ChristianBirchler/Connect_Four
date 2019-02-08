d3c50d58d59b95599bc99ae1473927d689e274f6
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-08 13:44:06 +0100

    implement user input functionality

--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -1,5 +1,6 @@
 package userInterface;
 import game.*;
+import java.util.Scanner;
 
 public abstract class UI {
 	
@@ -13,8 +14,31 @@ public abstract class UI {
 	
 	// METHODS
 	public abstract void printGameBoard();
+	
 	public void takeUserInput() {
+		int choice = -1;
 		
+		System.out.println("Enter a position: ");
+		choice = this.input();
+		
+		while(!this.validInput(choice)) {
+			System.out.println("Invalid input!\nEnter a position: ");
+			choice = this.input();
+		}
+		
+		
+	}
+	
+	// -- Implementation
+	private int input() {
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		int n = reader.nextInt(); // Scans the next token of the input as an int.
+	
+		return n;
+	}
+	
+	private boolean validInput(int n) {
+		return false;
 	}
 	
 }
