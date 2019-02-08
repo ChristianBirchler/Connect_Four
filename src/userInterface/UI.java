96e759af4f109d76a178a7c786e3ba8ce8ad763b
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-08 18:52:52 +0100

    make method abstract since CUI and GUI have different implementations

--- src/userInterface/UI.java
+++ src/userInterface/UI.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public abstract class UI {
 	
 	// ATTRIBUTES
-	private GameLogic gameLogic;
+	protected GameLogic gameLogic;
 	
 	// CONSTRUCTORS
 	public UI(GameLogic gl) {
@@ -15,30 +15,12 @@ public abstract class UI {
 	// METHODS
 	public abstract void printGameBoard();
 	
-	public void takeUserInput() {
-		int choice = -1;
-		
-		System.out.println("Enter a position: ");
-		choice = this.input();
-		
-		while(!this.validInput(choice)) {
-			System.out.println("Invalid input!\nEnter a position: ");
-			choice = this.input();
-		}
+	public abstract void takeUserInput();
 		
 		
-	}
+	
 	
 	// -- Implementation
-	private int input() {
-		Scanner reader = new Scanner(System.in);  // Reading from System.in
-		int n = reader.nextInt(); // Scans the next token of the input as an int.
 	
-		return n;
-	}
-	
-	private boolean validInput(int n) {
-		return false;
-	}
 	
 }
