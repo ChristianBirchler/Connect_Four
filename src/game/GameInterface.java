0fbdd4ae9fa00de34fba18373868cdc079011fb7
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-09 17:57:17 +0100

    avoid access to whole game board

--- src/game/GameInterface.java
+++ src/game/GameInterface.java
@@ -3,7 +3,9 @@ package game;
 public interface GameInterface {
 
 	public void putAt(int pos);
-	public GameBoard getGameBoard();
 	public Player getCurrentPlayer();
+	public int getXsize();
+	public int getYsize();
+	public boolean isFullAt(int pos);
 	
 }
