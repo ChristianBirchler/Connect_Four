711b6fd9a87975c75a3244d6ae39e25229107f51
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-11 21:50:38 +0100

    use position strings

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -1,18 +1,40 @@
 package game;
+import java.util.ArrayList;
 
 public class Cell {
 	/*
-	 * This class represents the cells of the game board and stores information about the status.
+	 * This class represents the cells of the game board and stores information about
+	 * the connections of the cells.
 	 */
 	
 	// ATTRIBUTES
 	private Player owner; // 'player' has occupied this cell
-	private int inLine; // stores the number of connected cells of player (updated by insertion)
+	
+	// stores the number of connected cells of player (updated by insertion)
+	// meaning: ul = "upper-left" ll = "lower-left"
+	
+	private int[] inline; // orientation: first item is upper-left then clock-wise
+	private ArrayList<String> posistions; // inline and positions have the same indices
 	
 	// CONSTRUCTOR
 	public Cell() {
 		this.owner = null;
-		this.inLine = 0;
+		
+		this.inline = new int[8];
+		for(int i=0;i<8;i++) {
+			this.inline[i] = 0;
+		}
+		
+		this.posistions = new ArrayList<String>();
+		this.posistions.add("upper-left");
+		this.posistions.add("upper");
+		this.posistions.add("upper-right");
+		this.posistions.add("right");
+		this.posistions.add("lower-right");
+		this.posistions.add("lower");
+		this.posistions.add("lower-left");
+		this.posistions.add("left");
+		
 	}
 	
 	// MEHTODS
@@ -25,12 +47,25 @@ public class Cell {
 		this.owner = pl;
 	}
 	
-	public int getInLine() {
-		return this.inLine;
+	public int getInLine(String pos) {
+		
+		// PRECONDITION: check if 'pos' is a valid position
+		if(!this.posistions.contains(pos)) {
+			throw new Error("The position is invalid!");
+		}
+		
+		int posIndex = this.posistions.indexOf(pos);
+		return this.inline[posIndex];
 	}
 	
-	public void incrInLine() {
-		this.inLine++;
+	public void setInLine(String pos, int val) {
+		// PRECONDITION: check if 'pos' is a valid position
+		if(!this.posistions.contains(pos)) {
+			throw new Error("The position is invalid!");
+		}
+		
+		int posIndex = this.posistions.indexOf(pos);
+		this.inline[posIndex] = val;
 	}
 	
 }
