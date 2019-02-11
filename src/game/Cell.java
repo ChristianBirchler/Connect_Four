163a0cafd79d6b3aed214dbcc8a7f413f353cf96
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-11 20:53:55 +0100

    implement class Cell

---
+++ src/game/Cell.java
@@ -0,0 +1,36 @@
+package game;
+
+public class Cell {
+	/*
+	 * This class represents the cells of the game board and stores information about the status.
+	 */
+	
+	// ATTRIBUTES
+	private Player owner; // 'player' has occupied this cell
+	private int inLine; // stores the number of connected cells of player (updated by insertion)
+	
+	// CONSTRUCTOR
+	public Cell() {
+		this.owner = null;
+		this.inLine = 0;
+	}
+	
+	// MEHTODS
+	// -- Access
+	public Player getOwner() {
+		return this.owner;
+	}
+	
+	public void setOwner(Player pl) {
+		this.owner = pl;
+	}
+	
+	public int getInLine() {
+		return this.inLine;
+	}
+	
+	public void increaseInLine() {
+		this.inLine++;
+	}
+	
+}
