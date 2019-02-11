3d761cffdafa3a778e18599850cdab07cbc89686
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-11 22:30:43 +0100

    add TestClass signature

---
+++ src/game/CellTest.java
@@ -0,0 +1,33 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CellTest {
+
+	@Test
+	void testCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetOwner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetOwner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInLine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetInLine() {
+		fail("Not yet implemented");
+	}
+
+}
