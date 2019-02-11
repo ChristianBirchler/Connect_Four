557aa009302ef91e14cd3181a3a7a537ffb30b49
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-11 15:45:22 +0100

    print character into the table

--- src/userInterface/CUI.java
+++ src/userInterface/CUI.java
@@ -1,6 +1,5 @@
 package userInterface;
 import java.util.Scanner;
-
 import game.*;
 
 public class CUI extends UI {
@@ -10,11 +9,14 @@ public class CUI extends UI {
 	
 	
 	// ATTRIBUTES
-	// TODO add symbols (chip representations) as attributes
+	private String pl1, pl2, empty;
 	
 	// CONSTRUCTOR
 	public CUI(GameInterface gi) {
 		super(gi);
+		this.pl1 = "X";
+		this.pl2 = "O";
+		this.empty = " ";
 	}
 	
 	// METHODS
@@ -29,15 +31,21 @@ public class CUI extends UI {
 		
 		int xsize = this.game.getXsize();
 		int ysize = this.game.getYsize();
+		String ch = "";
 		
+		// print the actual game board with nested loops
 		for(int row=ysize;row>0;row--) {
 			System.out.print("|");
-			for(int col=0;col<xsize;col++) {
-				System.out.print("_|");
+			for(int col=1;col<=xsize;col++) {
+				ch = this.getChar(col, row); // use setChar to determine the right character
+				System.out.print(ch);
+				System.out.print("|");
 			}
 			System.out.print("\n");
 		}
 		
+			
+		// print the slot numbers
 		for(int i=1;i<xsize+1;i++) {
 			System.out.print(" " + i);
 		}
@@ -73,6 +81,28 @@ public class CUI extends UI {
 		
 		return n;
 	}
+	
+	private String getChar(int xpos, int ypos) {
+		/*
+		 * This method determine and returns the character for the current cell.
+		 */
+		Player one = this.game.getPlayer1();
+		Player two = this.game.getPlayer2();
+		Player cell = this.game.getPlayerAt(xpos, ypos);
+		
+		if(cell == null) {
+			return this.empty;
+		}else if(cell == one) {
+			return this.pl1;
+		}else if(cell == two) {
+			return this.pl2;
+		}else {
+			throw new Error("The character could not be determined!");
+		}
+		
+		
+	}
+	
 	
 	
 }
