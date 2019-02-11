e9b6937ca15604a11d87498ee2f02956716cab93
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-11 15:46:30 +0100

    Player getter for the interface

--- src/game/GameInterface.java
+++ src/game/GameInterface.java
@@ -4,9 +4,11 @@ public interface GameInterface {
 
 	public void putAt(int pos);
 	public Player getCurrentPlayer();
+	public Player getPlayer1();
+	public Player getPlayer2();
+	public Player getPlayerAt(int xpos, int ypos);
 	public int getXsize();
 	public int getYsize();
-	//public boolean isFullAt(int pos);
 	public boolean validInput(int pos);
 	
 }
