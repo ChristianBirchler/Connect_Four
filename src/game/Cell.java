1df6f3cc991a5c42b5fe7788834161f8f2e7921e
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-12 21:38:17 +0100

    replace some unnecessary inline properties

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -10,30 +10,25 @@ public class Cell {
 	// ATTRIBUTES
 	private Player owner; // 'player' has occupied this cell
 	
-	// stores the number of connected cells of player (updated by insertion)
-	// meaning: ul = "upper-left" ll = "lower-left"
-	
-	private int[] inline; // orientation: first item is upper-left then clock-wise
-	private ArrayList<String> posistions; // inline and positions have the same indices
+	// stores the number of connected cells of player (updated by insertion)	
+	private int[] inline; // same indices like 'positions'
+	private ArrayList<String> positions; // stores the possible postition strings in a list
 	
 	// CONSTRUCTOR
 	public Cell() {
 		this.owner = null;
 		
-		this.inline = new int[8];
-		for(int i=0;i<8;i++) {
+		this.inline = new int[4];
+		for(int i=0;i<4;i++) {
 			this.inline[i] = 0;
 		}
 		
-		this.posistions = new ArrayList<String>();
-		this.posistions.add("upper-left");
-		this.posistions.add("upper");
-		this.posistions.add("upper-right");
-		this.posistions.add("right");
-		this.posistions.add("lower-right");
-		this.posistions.add("lower");
-		this.posistions.add("lower-left");
-		this.posistions.add("left");
+		this.positions = new ArrayList<String>();
+		this.positions.add("vertical");
+		this.positions.add("horizontal");
+		this.positions.add("diag-up");
+		this.positions.add("diag-down");
+		
 		
 	}
 	
@@ -50,21 +45,21 @@ public class Cell {
 	public int getInLine(String pos) {
 		
 		// PRECONDITION: check if 'pos' is a valid position
-		if(!this.posistions.contains(pos)) {
+		if(!this.positions.contains(pos)) {
 			throw new Error("The position is invalid!");
 		}
 		
-		int posIndex = this.posistions.indexOf(pos);
+		int posIndex = this.positions.indexOf(pos);
 		return this.inline[posIndex];
 	}
 	
 	public void setInLine(String pos, int val) {
 		// PRECONDITION: check if 'pos' is a valid position
-		if(!this.posistions.contains(pos)) {
+		if(!this.positions.contains(pos)) {
 			throw new Error("The position is invalid!");
 		}
 		
-		int posIndex = this.posistions.indexOf(pos);
+		int posIndex = this.positions.indexOf(pos);
 		this.inline[posIndex] = val;
 	}
 	
