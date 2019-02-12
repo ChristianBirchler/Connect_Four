0e84ae03111e8bcdcf46d388ce0fda0485c81367
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-12 21:45:35 +0100

    add base test structure

---
+++ src/game/GameBoardTest.java
@@ -0,0 +1,32 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameBoardTest {
+
+	@Test
+	void testGameBoard() {
+		
+		
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testThrowAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsFullAt() {
+		fail("Not yet implemented");
+	}
+
+}
