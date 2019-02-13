1fca99f88551dd2f0692f478392454fd6db65f84
Christian Birchler <christian.birchler2@uzh.ch>
2019-02-13 18:41:28 +0100

    test current player

--- src/game/GameLogicTest.java
+++ src/game/GameLogicTest.java
@@ -54,7 +54,69 @@ class GameLogicTest {
 
 	@Test
 	void testGetCurrentPlayer() {
-		fail("Not yet implemented");
+		
+		Player pl1 = new Player("Me");
+		Player pl2 = new Player("You");
+		GameBoard gb = new GameBoard(5, 5);
+		
+		GameLogic gl = new GameLogic(pl1,pl2,gb);
+		
+		// initial current player
+		assertEquals(pl1, gl.getCurrentPlayer(), "Player 1 should be the initial current player!");
+		
+		// normal case
+		gl.putAt(1);
+		assertEquals(pl2, gl.getCurrentPlayer(), "Player 2 should be the current player!");
+		gl.putAt(3);
+		assertEquals(pl1, gl.getCurrentPlayer(), "Player 1 should be the current player!");
+		
+		// full column case
+		gl.putAt(1);
+		gl.putAt(1);
+		gl.putAt(1);
+		gl.putAt(1);
+		
+		try {
+		gl.putAt(1); // should be invalid since column is already full
+		}catch(IllegalArgumentException e) {
+			System.out.println("IllegalArgumentException during test: " + e.getMessage());
+		}finally {
+		// the player should be the same after an invalid input
+		assertEquals(pl1, gl.getCurrentPlayer(),
+		"If column is full then the player 1 should be still the current player!");
+		}
+		
+		try {
+			gl.putAt(0);
+		}catch(IllegalArgumentException e) {
+			System.out.println("IllegalArgumentException during test: " + e.getMessage());
+		}finally {
+			assertEquals(pl1, gl.getCurrentPlayer(),
+			"If the argument is 0 then the current player should be still player 1!");
+		}
+		
+		
+		try {
+			gl.putAt(-2);
+		}catch(IllegalArgumentException e) {
+			System.out.println("IllegalArgumentException during test: " + e.getMessage());
+		}finally {
+			assertEquals(pl1, gl.getCurrentPlayer(),
+			"If the argument is -2 then the current player should be still player 1!");
+		}
+		
+		try {
+			gl.putAt(7);
+		}catch(IllegalArgumentException e) {
+			System.out.println("IllegalArgumentException during test: " + e.getMessage());
+		}finally {
+			assertEquals(pl1, gl.getCurrentPlayer(),
+			"If the argument is 7 then the current player should be still player 1!");
+		}
+	
+		
+		
+		
 	}
 
 	@Test
